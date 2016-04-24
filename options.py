5186f86723a0967f1f295b5161c8e5a605f67a9f
Sachin <sachin@gmail.com>
2016-04-24 10:05:10 -0700

    first pass

--- options.py
+++ options.py
@@ -1,18 +1,63 @@
+#!/usr/bin/python
 # Paramaters 
 base_price=100.00
 buy1_per=20.0
+buy2_per=40.0
 put_cost=5.0
+put_price=110.0
 call_gain=20.0
 print("Inital price: %02f"% base_price) 
 
 # Initial investment
+transact_cnt=0
 total_shares=0
 total_cost=0
+my_put=0
+my_call=0
 
 # Simulation 
+transact_cnt+=1
 cur_price=base_price
 cur_shares=33
 total_shares+=cur_shares
 total_cost+=(cur_price*cur_shares)
+print('Transcation #'+str(transact_cnt))
+print('Transact: Buy '+str(cur_shares)  +' shares @'+str(cur_price)+'$/share')
+print('Total Shares: '+str(total_shares)+' shares @'+str(total_cost/total_shares)+'$/share')
+
+# First buy
+transact_cnt+=1
+cur_price=base_price*(1+buy1_per/100.0)
+cur_shares=33
+total_shares+=cur_shares
+total_cost+=(cur_price*cur_shares)
+print('Transcation #'+str(transact_cnt))
 print('Transact: Buy '+str(cur_shares)+' shares @'+str(cur_price)+'$/share')
-print('Total Shares: '+str(total_shares)+' Total cost: '+str(total_cost)+'$')
+
+cur_put_base=put_price
+cur_put_price=put_cost
+my_put=cur_put_base
+total_cost+=(cur_put_price*100)
+print('Transact: Buy put @'+str(cur_put_price*100)+'$/100 shares')
+max_loss=my_put*total_shares-total_cost
+cur_gain=cur_price*total_shares-total_cost
+print('Total Shares: '+str(total_shares)+' shares @'+str(total_cost/total_shares)+'$/share')
+print('Min Loss %: '+str(100.0*max_loss/total_cost)+'% Max Loss '+str(max_loss))
+print('Cur Gain %: '+str(100.0*cur_gain/total_cost)+'% Gain Loss '+str(cur_gain))
+
+# Second buy
+transact_cnt+=1
+cur_price=base_price*(1+buy2_per/100.0)
+cur_shares=34
+total_shares+=cur_shares
+total_cost+=(cur_price*cur_shares)
+print('Transcation #'+str(transact_cnt))
+print('Transact: Buy '+str(cur_shares)+' shares @'+str(cur_price)+'$/share')
+print('Transact: Buy put @'+str(cur_put_price*100)+'$/100 shares')
+
+max_loss=my_put*total_shares-total_cost
+cur_gain=cur_price*total_shares-total_cost
+print('Total Shares: '+str(total_shares)+' shares @'+str(total_cost/total_shares)+'$/share')
+print('Min Loss %: '+str(100.0*max_loss/total_cost)+'% Max Loss '+str(max_loss))
+print('Cur Gain %: '+str(100.0*cur_gain/total_cost)+'% Gain Loss '+str(cur_gain))
+
