56ac649226a7fe237f52728f029dde1020d20965
Sachin <sachin@gmail.com>
2016-04-25 21:35:30 -0700

    added checkbox

--- option_gui.py
+++ option_gui.py
@@ -3,11 +3,14 @@ import Tkinter
 from Tkinter import *
 
 # -----------------------------------------
+# label1=Label(frame, text='Trasact #'+num)
 def trasact_menu(parent,frame,num):
-	label1=Label(frame, text='Trasact #'+num)
+	trans_bool=IntVar()
+	label1=Checkbutton(frame,text='Trasact #'+num,variable=trans_bool,width=8,onvalue=1,offvalue=0)
+	trans_bool.set(1)
 	label1.pack(side=LEFT)
 	trans_type=StringVar(frame)
-	trans_type.set("No action");
+	trans_type.set("No action")
 	choice1=OptionMenu(frame,trans_type,"No Action", "Buy Stock", "Buy Put  ", "Buy Call ")
 	choice1.pack(side=LEFT)
 	value1=Entry(frame,width=10)
